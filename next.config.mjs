625bc3190ed4c205c6fce7675f9fd6a2efcdd8fc
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-17 00:54:18 -0300

    :construction: finished auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**.googleusercontent.com",
+				port: "",
+			},
+		],
+	},
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
 
-export default nextConfig;
+export default nextConfig
