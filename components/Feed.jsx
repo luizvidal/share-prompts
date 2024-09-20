f7ca65560e2861ffffbd2c3882a3d9232b0c6875
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-20 18:50:23 -0300

    :construction: finished feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map((post) => (
+				<PromptCard
+					key={post._id}
+					post={post}
+					handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	);
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+	const [searchText, setSearchText] = useState("");
+	const [prompts, setPrompts] = useState([]);
 
-export default Feed
+	const handleSearchChange = (e) => {};
+
+	useEffect(() => {
+		const fetchPrompts = async () => {
+			const response = await fetch("/api/prompt");
+			const data = await response.json();
+
+			setPrompts(data);
+		};
+
+		fetchPrompts();
+	}, []);
+
+	return (
+		<section className="feed">
+			<form className="relative w-full flex-center">
+				<input
+					type="text"
+					placeholder="Search for a tag or a username"
+					value={searchText}
+					onChange={handleSearchChange}
+					required
+					className="search_input peer"
+				/>
+			</form>
+
+			<PromptCardList data={prompts} handleTagClick={() => {}} />
+		</section>
+	);
+};
+
+export default Feed;
