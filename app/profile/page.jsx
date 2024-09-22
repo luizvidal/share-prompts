720c47bfada6c0c262f45ebc1e6135d9bec79dd3
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2024-09-22 14:36:32 -0300

    :construction: finished ai prompt share project

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,13 +1,90 @@
-"use client";
+// "use client";
 
+// import Profile from "@components/Profile";
+// import { getSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// const Page = () => {
+// 	const router = useRouter();
+// 	const [posts, setPosts] = useState([]);
+
+// 	const handleEdit = (post) => {
+// 		router.push(`/update-prompt?id=${post._id}`);
+// 	};
+
+// 	const handleDelete = async (post) => {
+// 		const hasConfirmed = confirm(
+// 			"Are you sure you want to delete this prompt?"
+// 		);
+
+// 		if (hasConfirmed) {
+// 			try {
+// 				await fetch(`/api/prompt/${post._id.toString()}`, {
+// 					method: "DELETE",
+// 				});
+
+// 				const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+// 				setPosts(filteredPosts);
+// 			} catch (error) {
+// 				console.log(error);
+// 			}
+// 		}
+// 	};
+
+// 	useEffect(() => {
+// 		const fetchUserPrompts = async (userId) => {
+// 			const response = await fetch(`/api/users/${userId}/posts`);
+// 			const data = await response.json();
+
+// 			setPosts(data);
+// 		};
+
+// 		const fetchSession = async () => {
+// 			const session = await getSession();
+
+// 			if (session?.user.id) fetchUserPrompts(session.user.id);
+// 		};
+
+// 		fetchSession();
+// 	}, []);
+
+// 	return (
+// 		<Profile
+// 			name="My"
+// 			desc="Welcome to your personalized profile page"
+// 			data={posts}
+// 			handleEdit={handleEdit}
+// 			handleDelete={handleDelete}
+// 		/>
+// 	);
+// };
+
+// export default Page;
+
+"use client";
 import Profile from "@components/Profile";
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-const Page = () => {
-	const [posts, setPosts] = useState([]);
+const MyProfile = () => {
 	const router = useRouter();
+	const { data: session } = useSession();
+
+	const [myPosts, setMyPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`/api/users/${session?.user.id}/posts`);
+			const data = await response.json();
+
+			setMyPosts(data);
+		};
+
+		if (session?.user.id) fetchPosts();
+	}, [session?.user.id]);
 
 	const handleEdit = (post) => {
 		router.push(`/update-prompt?id=${post._id}`);
@@ -24,41 +101,24 @@ const Page = () => {
 					method: "DELETE",
 				});
 
-				const filteredPosts = posts.filter((p) => p._id !== post._id);
+				const filteredPosts = myPosts.filter((item) => item._id !== post._id);
 
-				setPosts(filteredPosts);
+				setMyPosts(filteredPosts);
 			} catch (error) {
 				console.log(error);
 			}
 		}
 	};
 
-	useEffect(() => {
-		const fetchUserPrompts = async (userId) => {
-			const response = await fetch(`/api/users/${userId}/posts`);
-			const data = await response.json();
-
-			setPosts(data);
-		};
-
-		const fetchSession = async () => {
-			const session = await getSession();
-
-			if (session?.user.id) fetchUserPrompts(session.user.id);
-		};
-
-		fetchSession();
-	}, []);
-
 	return (
 		<Profile
 			name="My"
-			desc="Welcome to your personalized profile page"
-			data={posts}
+			desc="Welcome to your personalized profile page. Share your exceptional prompts and be inspired by others."
+			data={myPosts}
 			handleEdit={handleEdit}
 			handleDelete={handleDelete}
 		/>
 	);
 };
 
-export default Page;
+export default MyProfile;
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,21 +18,53 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-	const [searchText, setSearchText] = useState("");
-	const [prompts, setPrompts] = useState([]);
+	const [allPosts, setAllPosts] = useState([]);
 
-	const handleSearchChange = (e) => {};
+	// Search states
+	const [searchText, setSearchText] = useState("");
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchedResults, setSearchedResults] = useState([]);
+
+	const fetchPosts = async () => {
+		const response = await fetch("/api/prompt");
+		const data = await response.json();
+
+		setAllPosts(data);
+	};
 
 	useEffect(() => {
-		const fetchPrompts = async () => {
-			const response = await fetch("/api/prompt");
-			const data = await response.json();
-
-			setPrompts(data);
-		};
-
-		fetchPrompts();
+		fetchPosts();
 	}, []);
+
+	const filterPrompts = (searchText) => {
+		const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
+		return allPosts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	};
+
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		//debounce method
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = filterPrompts(e.target.value);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
+
+	const handleTagClick = (tagName) => {
+		setSearchText(tagName);
+
+		const searchResult = filterPrompts(tagName);
+		setSearchedResults(searchResult);
+	};
 
 	return (
 		<section className="feed">
@@ -47,7 +79,14 @@ const Feed = () => {
 				/>
 			</form>
 
-			<PromptCardList data={prompts} handleTagClick={() => {}} />
+			{searchText ? (
+				<PromptCardList
+					data={searchedResults}
+					handleTagClick={handleTagClick}
+				/>
+			) : (
+				<PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+			)}
 		</section>
 	);
 };
